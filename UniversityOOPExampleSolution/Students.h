9caf13ecff2d2143fff38eefed00a5b1e0e7bd51
Gusenov <iserveriazzotov22@gmail.com>
2024-03-27 18:49:04 +0300

    Added default constructor

--- UniversityOOPExampleSolution/Students.h
+++ UniversityOOPExampleSolution/Students.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include <string>
 using namespace std;
 
@@ -8,8 +8,16 @@ public:
 	int age;
 	double mark;
 
+	// default consrtuctor (конструктор по умолчанию)
+	Student() {
+		cout << "Default consrtuctor is calling" << endl;
+		name = "no name";
+		age = 6;
+		mark = 4;
+	}
+
 	string convert() {
-		string s = " ";
+		string s = "";
 		s += name;
 		s += ": age = " + to_string(age);
 		s += ", mark = " + to_string(mark);
