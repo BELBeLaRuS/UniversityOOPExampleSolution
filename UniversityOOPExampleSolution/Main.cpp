fa7c0e6653fd6b5c08f30c21ef4202d0737cae27
Gusenov <iserveriazzotov22@gmail.com>
2024-04-10 18:41:43 +0300

    Use objects pointers

--- UniversityOOPExampleSolution/Main.cpp
+++ UniversityOOPExampleSolution/Main.cpp
@@ -4,8 +4,14 @@
 int main() {
 	Student st{ "Alex", 14, 9 };
 
+	cout << st.convert() << endl;
 
+	Student* point_st = new Student();
 
+	cout << point_st->convert() << endl;
+	
+
+	delete point_st;
 
 	return 0;
 }
