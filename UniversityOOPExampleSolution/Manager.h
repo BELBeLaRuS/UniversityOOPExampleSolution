b3356c1e8f01ecb3f1bee636afe92aa46e7078e5
Gusenov <iserveriazzotov22@gmail.com>
2024-04-10 18:28:25 +0300

    (изменение изменения класса Manager на интерфейсную часть и представление) change change Manager class to frontend and view

--- UniversityOOPExampleSolution/Manager.h
+++ UniversityOOPExampleSolution/Manager.h
@@ -1,73 +1,15 @@
 #pragma once
 #include "Students.h"
-using namespace std;
 
 class Manager {
 public: 
-	double getMaxMark(Student* list, int length) {
-		double max = list[0].getMark;
+	double getMaxMark(Student* list, int length);
 
-		for (int i = 1; i < length; i++)
-		{
-			if (list[i].getMark > max) {
-				max = list[i].getMark;
-			}
-		}
+	double getMinMark(Student* list, int length);
 
-		return max;
-	}
+	double CalcAvgMark(Student* list, int length);
 
-	double getMinMark(Student* list, int length) {
-		double min = list[0].getMark;
+	Student getBestStudent(Student* list, int length);
 
-		for (int i = 1; i < length; i++)
-		{
-			if (list[i].getMark < min) {
-				min = list[i].getMark;
-			}
-		}
-
-		return min;
-	}
-
-	double CalcAvgMark(Student* list, int length) {
-		double avg = 0;
-
-		for (int i = 0; i < length; i++)
-		{
-			avg += list[i].getMark;
-		}
-
-		return avg / length;
-	}
-
-	Student getBestStudent(Student* list, int length) {
-		double mark = getMaxMark(list, length);
-		Student st;
-
-		for (int i = 0; i < length; i++)
-		{
-			if (list[i].getMark == mark) {
-				st = list[i];
-				break;
-			}
-		}
-
-		return st;
-	}
-
-	Student getWorstStudent(Student* list, int length) {
-		double mark = getMinMark(list, length);
-		Student st;
-
-		for (int i = 0; i < length; i++)
-		{
-			if (list[i].getMark == mark) {
-				st = list[i];
-				break;
-			}
-		}
-
-		return st;
-	}
+	Student getWorstStudent(Student* list, int length);
 };
